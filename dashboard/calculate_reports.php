c32cbcfabd145d52b3b2e67f4565cc98a98f0f78
ericsoi <ericksoi3709@gmail.com>
2023-10-24 17:47:36 +0300

    reports

--- dashboard/calculate_reports.php
+++ dashboard/calculate_reports.php
@@ -14,7 +14,8 @@ if(ISSET($_POST['save'])){
     echo $endDate;
     $db=new db_class();
     if ($_POST["status"]=="Complete Loans"){
-        $sum_payment=$db->conn->query("SELECT SUM(paid_amount), SUM(totalAmount), SUM(amount), count(*) FROM loan WHERE  paid_amount >= totalAmount AND (date_released BETWEEN '$startDate' AND '$endDate')");
+        // $sum_payment=$db->conn->query("SELECT SUM(paid_amount), SUM(totalAmount), SUM(amount), count(*) FROM loan WHERE  paid_amount >= totalAmount AND (date_released BETWEEN '$startDate' AND '$endDate')");
+        $sum_payment=$db->conn->query("SELECT SUM(paid_amount), SUM(totalAmount), SUM(amount), count(*) FROM loan_test WHERE CAST(paid_amount AS DECIMAL) >= CAST(totalAmount AS DECIMAL) AND date_released >= CAST('$startDate' AS DATE) AND date_released <= CAST('$endDate' AS DATE)");
         $sum_fetch=$sum_payment->fetch_array();
         print_r($sum_fetch);
         if ($sum_fetch[3]>0){
@@ -28,7 +29,9 @@ if(ISSET($_POST['save'])){
         }
     }
     if ($_POST["status"]=="Active Loans"){
-        $sum_payment=$db->conn->query("SELECT SUM(paid_amount), SUM(totalAmount), SUM(amount), count(*) FROM loan WHERE  paid_amount < totalAmount AND (date_released BETWEEN '$startDate' AND '$endDate')");
+        // $sum_payment=$db->conn->query("SELECT SUM(paid_amount), SUM(totalAmount), SUM(amount), count(*) FROM loan WHERE  paid_amount < totalAmount AND (date_released BETWEEN '$startDate' AND '$endDate')");
+        $sum_payment=$db->conn->query("SELECT SUM(paid_amount), SUM(totalAmount), SUM(amount), count(*) FROM loan_test WHERE CAST(paid_amount AS DECIMAL) < CAST(totalAmount AS DECIMAL) AND date_released >= CAST('$startDate' AS DATE) AND date_released <= CAST('$endDate' AS DATE)");
+
         $sum_fetch=$sum_payment->fetch_array();
         $sum_amount_out = $sum_fetch[2];
         $sum_totalAmount_out = $sum_fetch[1];
