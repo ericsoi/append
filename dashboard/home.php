557f83617e7643309dc464d1745c34cc6273ec18
ericsoi <ericksoi3709@gmail.com>
2023-11-07 14:26:15 +0300

    date

--- dashboard/home.php
+++ dashboard/home.php
@@ -4,7 +4,7 @@
 	require_once'class.php';
 	$db=new db_class(); 
     $currentDate = date("Y-m-d");
-    $newDate = date("Y-m-d", strtotime($currentDate . " -5 hours"));
+    $newDate = date("Y-m-d", strtotime($currentDate . " -6 hour"));
     
 ?>
 <!DOCTYPE html>
