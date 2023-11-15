a7d13dd533e772aefb3a8193abba435abc78dcfa
ericsoi <ericksoi3709@gmail.com>
2023-11-15 04:22:35 +0300

    mail

--- cronmail.php
+++ cronmail.php
@@ -134,8 +134,8 @@ try {
 
     //Recipients
     $mail->setFrom('matrickcredit@gmail.com', 'Matrick Credit');
-    $mail->addAddress('ericksoi3709@gmail.com', 'Matrick Developer');     //Add a recipient
-    // $mail->addAddress('kyalomartin1990@gmail.com', 'Matrick Admin');     //Add a recipient
+    // $mail->addAddress('ericksoi3709@gmail.com', 'Matrick Developer');     //Add a recipient
+    $mail->addAddress('kyalomartin1990@gmail.com', 'Matrick Admin');     //Add a recipient
 
     $mail->addReplyTo('matrickcredit@gmail.com', 'Matrick Credit');
 
