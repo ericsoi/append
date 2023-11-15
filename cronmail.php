17bf1845752006b9f2e05cc3b92146d60e1ff186
ericsoi <ericksoi3709@gmail.com>
2023-11-15 03:46:50 +0300

    mail

--- cronmail.php
+++ cronmail.php
@@ -5,17 +5,11 @@
     <title>Bootstrap Table Example</title>
     <!-- Add Bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
 </head>
 <body>
+
 <?php
-require_once'/var/www/matrick/dashboard/class.php';
-$db=new db_class(); 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-$tbl_unpaid = $db->conn->query("SELECT * from `loan` INNER JOIN `borrower` on borrower.borrower_id = loan.borrower_id INNER JOIN `loan_plan` on loan_plan.lplan_id = loan.lplan_id WHERE NOT `loan_id` IN (SELECT `loan_id` FROM `payment` WHERE DATE(`date_created`) = DATE(DATE_SUB(NOW(), INTERVAL 13 HOUR)))
-AND `status` IS NOT NULL");$tbl_unpaid_yesterday = $db->conn->query("SELECT * from `loan` INNER JOIN `borrower` on borrower.borrower_id = loan.borrower_id INNER JOIN  `loan_plan` on loan_plan.lplan_id = loan.lplan_id where NOT `loan_id` in (select `loan_id` from `payment` WHERE DATE(`date_created`) = DATE(DATE_SUB(NOW(), INTERVAL 1 DAY))) AND `status` IS NOT NULL");
+// Your PHP code for table generation goes here...
 
 // Check if there are rows returned from the query
 if ($tbl_unpaid->num_rows > 0) {
@@ -28,11 +22,14 @@ if ($tbl_unpaid->num_rows > 0) {
     }
 
     // Create a table with the fetched data
-    $tableHtml = '<div class="table-responsive"><table class="table table-success table-bordered table-striped table-hover">';
+    $tableHtml = '<div class="container mt-4">';
+    $tableHtml .= '<h2>Unpaid Loans</h2>';
+    $tableHtml .= '<div class="table-responsive">';
+    $tableHtml .= '<table class="table table-bordered table-striped">';
     $tableHtml .= '<thead class="thead-light"><tr><th scope="col">Names</th><th scope="col">Contact</th><th scope="col">Contact_2</th><th scope="col">Address</th><th scope="col">Date Approved</th></tr></thead><tbody>';
 
     foreach ($databaseData as $row) {
-        $tableHtml .= '<tr scope="row">';
+        $tableHtml .= '<tr>';
         $tableHtml .= '<td>' . $row['firstname'] . ' ' . $row['middlename'] .' '.$row['lastname'] . '</td>';
         $tableHtml .= '<td>' . $row['contact_no'] . '</td>';
         $tableHtml .= '<td>' . $row['email'] . '</td>';
@@ -41,11 +38,12 @@ if ($tbl_unpaid->num_rows > 0) {
         $tableHtml .= '</tr>';
     }
 
-    $tableHtml .= '</tbody></table></div>';
+    $tableHtml .= '</tbody></table></div></div>';
 } else {
     // Handle the case when no rows are returned from the query
     $tableHtml = '<div class="alert alert-warning" role="alert">No data found.</div>';
 }
+
 
 if ($tbl_unpaid_yesterday->num_rows > 0) {
     // Initialize an empty array to store the fetched data
